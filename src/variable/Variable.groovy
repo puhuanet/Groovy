ad776c1740ac5f09080d673db5d8c4e5f2c013a8
oliver-csu <oliver.csu@gmail.com>
2020-01-01 22:38:05 +0800

    variable

---
+++ src/variable/Variable.groovy
@@ -0,0 +1,31 @@
+package variable
+
+// int类型
+int i = 1
+println i
+println i.class
+
+// double类型
+double d = 3.14
+println d
+println d.class
+
+// String类型
+String s = "string"
+println s
+println s.class
+
+// def int类型
+def di = 1
+println di
+println di.class
+
+// def double类型
+double dd = 3.14
+println dd
+println dd.class
+
+// def String类型
+String ds = "string"
+println ds
+println ds.class
