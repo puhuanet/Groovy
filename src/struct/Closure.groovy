b4322803b30e4c4ad3d9bd2e1b08425f43a9b409
oliver-csu <oliver.csu@gmail.com>
2020-01-21 15:00:26 +0800

    闭包使用

--- src/struct/Closure.groovy
+++ src/struct/Closure.groovy
@@ -54,3 +54,32 @@ clouser = {
 println clouser.call()
 // 直接调用
 println clouser()
+
+int x=5
+
+// 求阶乘-upto()方法
+int fab1(int number) {
+    int result = 1
+    result.upto(number, {num -> result *= num})
+    return result
+}
+println fab1(x)
+
+// 求阶乘-downto()方法
+int fab2(int number) {
+    int result = 1
+    number.downto(1) {
+        num -> result *= num
+    }
+    return result
+}
+println fab2(x)
+
+int cal(int number) {
+    int result = 0
+    number.times {
+        num -> result += num
+    }
+    return result
+}
+println cal(x)
