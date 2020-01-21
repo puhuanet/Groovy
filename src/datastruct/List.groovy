cbe9d849c611f5edfe8b61fbee60bcf7d7ebd532
oliver-csu <oliver.csu@gmail.com>
2020-01-21 17:38:39 +0800

    List的定义

--- src/datastruct/List.groovy
+++ src/datastruct/List.groovy
@@ -4,3 +4,10 @@ package datastruct
 def array1 = [1,2,3,4,5] as int[]
 int[] array2 = [1,2,3,4,5]
 
+// List的定义
+def list1 = []
+println list1.class
+def list2 = [1, 2, 3, 4, 5]
+println list2.size()
+
+
