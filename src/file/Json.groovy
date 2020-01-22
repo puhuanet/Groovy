932bcda90a1f864447c848b4880c0b8395bda8de
oliver-csu <oliver.csu@gmail.com>
2020-01-22 15:17:50 +0800

    Get Network Json Data

--- src/file/Json.groovy
+++ src/file/Json.groovy
@@ -23,3 +23,17 @@ println list
 list = slurper.parse(json.getBytes())
 println list
 
+// 从网络上获取数据
+def getNetworkData(String url) {
+    // 发送http请求
+    def connection = new URL(url).openConnection()
+    connection.setRequestMethod('GET')
+    connection.connect()
+    def response = connection.content.text
+    // 将json转化为实体对象
+    def jsonSluper = new JsonSlurper()
+    return jsonSluper.parseText(response)
+}
+
+// 输出获取到的数据
+println getNetworkData('https://www.baidu.com/home/xman/data/tipspluslist').data
